3ce40b8c7f3835c4ed30bb763a80b25eebe5a587
qianlu.kk <qianlu.lk@alibaba-inc.com>
2022-11-25 18:02:06 +0800

    implements opentelemetry ostream exporter demo

---
+++ WORKSPACE
@@ -0,0 +1,70 @@
+workspace(name = "ot-demo")
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+http_archive(
+    name = "io_opentelemetry_cpp",
+    sha256 = "2ad0911cdc94fe84a93334773bef4789a38bd1f01e39560cabd4a5c267e823c3",
+    strip_prefix = "opentelemetry-cpp-1.7.0",
+    urls = [
+        "https://github.com/open-telemetry/opentelemetry-cpp/archive/refs/tags/v1.7.0.tar.gz"
+    ],
+)
+
+# Load OpenTelemetry dependencies after load.
+load("@io_opentelemetry_cpp//bazel:repository.bzl", "opentelemetry_cpp_deps")
+
+opentelemetry_cpp_deps()
+
+# Load gRPC dependencies after load.
+load("@com_github_grpc_grpc//bazel:grpc_deps.bzl", "grpc_deps")
+
+grpc_deps()
+
+# Load extra gRPC dependencies due to https://github.com/grpc/grpc/issues/20511
+load("@com_github_grpc_grpc//bazel:grpc_extra_deps.bzl", "grpc_extra_deps")
+
+grpc_extra_deps()
+
+
+### foreign_cc & cmake etc
+#load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+#
+## Rule repository, note that it's recommended to use a pinned commit to a released version of the rules
+#http_archive(
+#   name = "rules_foreign_cc",
+#   sha256 = "5303e3363fe22cbd265c91fce228f84cf698ab0f98358ccf1d95fba227b308f6",
+#   strip_prefix = "rules_foreign_cc-0.9.0",
+#   url = "https://github.com/bazelbuild/rules_foreign_cc/archive/0.9.0.zip",
+#)
+
+#load("@rules_foreign_cc//foreign_cc:repositories.bzl", "rules_foreign_cc_dependencies")
+#
+## This sets up some common toolchains for building targets. For more details, please see
+## https://github.com/bazelbuild/rules_foreign_cc/tree/main/docs#rules_foreign_cc_dependencies
+#rules_foreign_cc_dependencies()
+#
+#_ALL_CONTENT = """\
+#filegroup(
+#    name = "all_srcs",
+#    srcs = glob(["**"]),
+#    visibility = ["//visibility:public"],
+#)
+#"""
+#
+#load("@bazel_tools//tools/build_defs/repo:git.bzl", "new_git_repository")
+#
+## pcre source code repository
+#new_git_repository(
+#    remote = "https://github.com/open-telemetry/opentelemetry-cpp.git",
+#    commit = "8bfb9a3f2ded4c8fcbb6a5ba786eebfd20452734",
+#    init_submodules = True,
+#    recursive_init_submodules = True,
+#    name = "opentelemetry_cpp_sdk",
+#    build_file_content = _ALL_CONTENT,
+#    strip_prefix = "opentelemetry-cpp",
+##    strip_prefix = "opentelemetry-cpp-1.7.0",
+##    urls = [
+##        "https://github.com/open-telemetry/opentelemetry-cpp/archive/refs/tags/v1.7.0.tar.gz"
+##    ],
+##    sha256 = "2ad0911cdc94fe84a93334773bef4789a38bd1f01e39560cabd4a5c267e823c3",
+#)
---
+++ src/main.cc
@@ -0,0 +1,82 @@
+//
+// Created by qianlu on 2022/11/24.
+//
+
+#include "opentelemetry/sdk/trace/batch_span_processor_factory.h"
+#include "opentelemetry/exporters/ostream/span_exporter_factory.h"
+#include "opentelemetry/trace/provider.h"
+#include "opentelemetry/sdk/trace/tracer_provider_factory.h"
+//#include "opentelemetry/exporters/otlp/otlp_grpc_exporter_factory.h"
+//#include "opentelemetry/exporters/otlp/"
+//#include "exporters/otlp/otlp_grpc_exporter/"
+
+int init_opentelemetry() {
+    // initialize grpc exporter
+//    opentelemetry::exporter::otlp::OtlpGrpcExporterOptions opts;
+//    opts.endpoint = "tracing-analysis-dc-bj.aliyuncs.com:8090";
+//    auto exporter = opentelemetry::exporter::otlp::OtlpGrpcExporterFactory::Create(opts);
+
+    auto exporter = opentelemetry::exporter::trace::OStreamSpanExporterFactory::Create();
+    // initialize batch span processor
+    opentelemetry::sdk::trace::BatchSpanProcessorOptions bsp_opts;
+    bsp_opts.schedule_delay_millis = std::chrono::milliseconds(15000); // 15ms
+    bsp_opts.max_export_batch_size = 1024;
+    bsp_opts.max_queue_size = 4096;
+    auto processor =
+            opentelemetry::sdk::trace::BatchSpanProcessorFactory::Create(std::move(exporter), bsp_opts);
+
+    // initialize trace provider
+    std::shared_ptr<opentelemetry::trace::TracerProvider> provider =
+            opentelemetry::sdk::trace::TracerProviderFactory::Create(
+                    std::move(processor),
+                    opentelemetry::sdk::resource::Resource::Create({
+                           {"", ""},
+                    }));
+
+    // set global trace provider
+    opentelemetry::trace::Provider::SetTracerProvider(provider);
+}
+
+opentelemetry::nostd::shared_ptr<opentelemetry::trace::Tracer> get_tracer() {
+    return opentelemetry::trace::Provider::GetTracerProvider()->GetTracer("qianlu", "v1.0.0");
+}
+
+int main() {
+    init_opentelemetry();
+
+    for (int i = 0; i < 1000; ++i) {
+        if (i / 2) {
+            // case 1: has traceId & spanId
+            std::array<const uint8_t, 16> tid = {'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+            std::array<const uint8_t, 8> sid = {'a', 1, 2, 3, 4, 5, 6, 7};
+            opentelemetry::trace::TraceId traceId(tid);
+            opentelemetry::trace::SpanId spanId(sid);
+            opentelemetry::trace::StartSpanOptions startSpanOptions;
+            startSpanOptions.kind = opentelemetry::trace::SpanKind::kServer;
+            // set trace parent
+            auto spanContext = opentelemetry::trace::SpanContext(
+                    traceId,
+                    spanId,
+                    opentelemetry::trace::TraceFlags(1),
+                    false);
+            startSpanOptions.parent = spanContext;
+            auto span = get_tracer()->StartSpan("spanName-1-child", startSpanOptions);
+
+        } else {
+            // case 2: no traceId & spanId
+            opentelemetry::trace::StartSpanOptions startSpanOptions1;
+            startSpanOptions1.kind = opentelemetry::trace::SpanKind::kServer;
+            auto parentSpan = get_tracer()->StartSpan("spanName-case2-parent", startSpanOptions1);
+            auto spanContext = parentSpan->GetContext();
+
+            opentelemetry::trace::StartSpanOptions startSpanOptions2;
+            startSpanOptions2.kind = opentelemetry::trace::SpanKind::kClient;
+            // set parent span (inherit parent span's traceId & spanId)
+            startSpanOptions2.parent = spanContext;
+            auto childSpan = get_tracer()->StartSpan("spanName-case2-child", startSpanOptions2);
+
+        }
+    }
+
+    return 0;
+}
